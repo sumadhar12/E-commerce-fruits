bd6144f7c64445c902fef7255871f88741a49299
sumadhar12 <147581711+sumadhar12@users.noreply.github.com>
2025-10-10 19:42:19 +0530

    Update adminlogin.js

--- client/src/pages/adminlogin.js
+++ client/src/pages/adminlogin.js
@@ -14,7 +14,7 @@ export default function AdminLogin() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/admin/login', {
+      const response = await fetch('https://e-commerce-fruits-backend.vercel.app/api/admin/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
